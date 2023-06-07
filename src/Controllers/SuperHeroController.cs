bebab9d34090cbe146f98f63ed8325ba9c7f91f7
Eder Soares Sena <eder.sena@live.com>
2023-06-07 17:40:16 -0300

    Get All but now from the real db

--- src/Controllers/SuperHeroController.cs
+++ src/Controllers/SuperHeroController.cs
@@ -24,10 +24,16 @@ namespace src.Controllers
             }
         };
 
+         private readonly DataContext _context;
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAll()
         {
-            return Ok(heroes);
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
         [HttpGet("{id}")]
@@ -73,5 +79,54 @@ namespace src.Controllers
 
             return Ok(heroToDelete);
         }
+        // [HttpGet]
+        // public async Task<ActionResult<List<SuperHero>>> GetAll()
+        // {
+        //     return Ok(heroes);
+        // }
+
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<SuperHero>> GetById(int id)
+        // {
+        //     var hero = heroes.FirstOrDefault(h => h.Id == id);
+        //     if (hero == null)
+        //         return NotFound("Hero not found.");
+            
+        //     return Ok(hero);
+        // }
+
+        // [HttpPost]
+        // public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        // {
+        //     heroes.Add(hero);
+        //     return Ok(heroes);
+        // }
+
+        // [HttpPut("{id}")]
+        // public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero, int id)
+        // {
+        //     var heroToUpdate = heroes.FirstOrDefault(h => h.Id == id);
+        //     if (heroToUpdate == null)
+        //         return NotFound("Hero not found.");
+            
+        //     heroToUpdate.Name = hero.Name;
+        //     heroToUpdate.FirstName = hero.FirstName;
+        //     heroToUpdate.LastName = hero.LastName;
+        //     heroToUpdate.Place = hero.Place;
+
+        //     return Ok(heroes);
+        // }
+
+        // [HttpDelete("{id}")]
+        // public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        // {
+        //     var heroToDelete = heroes.FirstOrDefault(h => h.Id == id);
+        //     if (heroToDelete == null)
+        //         return NotFound("Hero not found.");
+            
+        //     heroes.Remove(heroToDelete);
+
+        //     return Ok(heroToDelete);
+        // }
     }
 }
